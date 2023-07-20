ea4c7740504562839a78627209017a6ba74e262b
keerthanasriniv <117429428+keerthanasriniv@users.noreply.github.com>
2023-07-20 10:32:43 +0530

    Add files via upload

---
+++ Day9/src/NavComp/SlideBarData1.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+import * as FaIcons from "react-icons/fa";
+import * as AiIcons from "react-icons/ai";
+import * as IoIcons from "react-icons/io";
+// import Result from './Result.js';
+export const SidebarData = [
+  {
+    title: "Student DB",
+    path: "/StuDb",
+ 
+    cName: "nav-text"
+  },
+  {
+    title: "Result Management",
+    path: "/AdminRes",
+   
+    cName: "nav-text"
+  },
+  {
+    title: "Courses",
+    path: "/AdminCourses",
+   
+    cName: "nav-text"
+  },
+  {
+    title: "Attendence Management",
+    path: "/AdminAtt",
+   
+    cName: "nav-text"
+  },
+  {
+    title: "Grade Evaluation",
+    path: "/Grade",
+   
+    cName: "nav-text"
+  }
+];
